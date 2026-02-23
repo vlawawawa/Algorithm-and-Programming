d72f46fb8f1e52749377b63be47bddab5bd83d8b
vlawawawa <turtlepen1@gmail.com>
2026-02-24 06:45:33 +0700

    Add files via upload

---
+++ Basic_In_Out.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	
+	// Declare
+		int A;
+		char S[101]; // 100 (+1 for the null terminator)
+
+	// Input
+		scanf ("%s", S);
+		scanf ("%d", &A);
+	
+	// Output
+		printf ("%s\n", S);
+		printf ("%d\n", A);
+	
+	
+	return 0;
+}
